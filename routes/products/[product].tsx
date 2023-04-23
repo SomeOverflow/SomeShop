2e551e370e2e5952d3fb79bd1364ccb9c082f441
SomeOverflow <someoverflow@someworkflow.de>
2023-04-23 12:46:46 +0000

    feat: Add the product info
    Visualize the product that got fetched

--- routes/products/[product].tsx
+++ routes/products/[product].tsx
@@ -20,15 +20,19 @@ interface Product {
 
 export const handler: Handlers<PageData> = {
   async GET(_req, ctx) {
-    const result: PageData = { product: null };
-
     const data = await fetch(
       `https://fakestoreapi.com/products/${ctx.params.product}`,
     )
       .then((res) => res.json())
       .catch(() => {
-        return result;
+        return null;
       });
+
+    const result: PageData = { product: null };
+
+    if (data == null) {
+      return ctx.render(result);
+    }
 
     result.product = {
       id: data.id,
@@ -48,7 +52,7 @@ export const handler: Handlers<PageData> = {
 };
 
 export default function Product(props: PageProps<PageData>) {
-  const product = props.data.product;
+  const product: null | Product = props.data.product;
 
   return (
     <>
@@ -56,8 +60,63 @@ export default function Product(props: PageProps<PageData>) {
         <title>SomeShop - Product</title>
       </Head>
 
-      {product && <>Product</>}
-      {!product && <>404</>}
+      {product && (
+        <>
+          <section class="min-w-screen min-h-screen bg-white flex items-center p-5 lg:p-10 overflow-hidden relative">
+            <div class="w-full max-w-6xl rounded bg-white shadow-2xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+              <div class="md:flex items-center -mx-10">
+                <div class="w-full md:w-1/2 px-10 mb-10 md:mb-0">
+                  <div class="relative">
+                    <img
+                      src={product.image}
+                      class="object-scale-down h-[500px] w-full relative z-10"
+                      alt="Product Image"
+                    />
+                    <div class="border-4 border-yellow-200 absolute top-10 bottom-10 left-10 right-10 z-0">
+                    </div>
+                  </div>
+                </div>
+                <div class="w-full md:w-1/2 px-10">
+                  <div class="mb-10">
+                    <h1 class="font-bold uppercase text-2xl mb-5">
+                      {product.title}
+                    </h1>
+                    <p class="text-sm">
+                      {product.description}
+                    </p>
+                  </div>
+                  <div>
+                    <div class="inline-block align-bottom mr-5">
+                      <span class="text-2xl leading-none align-baseline">
+                        $
+                      </span>
+                      <span class="font-bold text-5xl leading-none align-baseline">
+                        {product.price.toString().split(".")[0]}
+                      </span>
+                      {product.price.toString().split(".")[1] &&
+                        (
+                          <span class="text-2xl leading-none align-baseline">
+                            .{product.price.toString().split(".")[1]}
+                          </span>
+                        )}
+                    </div>
+                    <div class="inline-block align-bottom">
+                      <button class="bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
+                        <i class="mdi mdi-cart -ml-2 mr-2"></i> BUY NOW
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
+      )}
+      {!product && (
+        <>
+          404
+        </>
+      )}
     </>
   );
 }
