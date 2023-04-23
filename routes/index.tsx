77ee9ac214dd86abb82505332a7e27930b2f243a
SomeOverflow <someoverflow@someworkflow.de>
2023-04-23 12:08:14 +0000

    feat: Fetch products
    Use a handler to pass products to the page props

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,61 @@
 import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { Handlers, PageProps } from "https://deno.land/x/fresh@1.1.5/server.ts";
 
-export default function Home() {
+interface PageData {
+  products: Product[];
+}
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+export const handler: Handlers<PageData> = {
+  async GET(_req, ctx) {
+    const amount = 25;
+    const data = await fetch(
+      `https://fakestoreapi.com/products?limit=${amount}`,
+    ).then((res) => res.json());
+
+    const result: PageData = { products: [] };
+
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        const element = data[key];
+        result.products.push({
+          id: element.id,
+          title: element.title,
+          price: element.price,
+          description: element.description,
+          category: element.category,
+          image: element.image,
+          rating: {
+            rate: element.rating.rate,
+            count: element.rating.count,
+          },
+        });
+      }
+    }
+
+    return ctx.render(result);
+  },
+};
+
+export default function Home(props: PageProps<PageData>) {
   return (
     <>
       <Head>
         <title>SomeShop</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <Counter start={3} />
-      </div>
+
     </>
   );
 }
